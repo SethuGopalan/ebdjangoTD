bbe1eeb2883ca1cb283e44618a0529ad9fd0fa06
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-30 09:09:30 -0400

    admin register done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 
-# Register your models here.
+from  blog.models import Profile,Post,Tag
+
+@admin.register(Profile)
+class ProfileAdmin(admin,ModelAdmin):
+    model = Profile
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display =(
+
+        "id",
+        "title",
+        "subtitle",
+        "slug",
+        "publish_data",
+        "published",
+    )
+    list_filter= (
+        "published",
+        "publish_data",
+
+    )
+    list_editable =(
+
+        "title",
+        "subtitle",
+        "slug",
+        "published_date",
+        "published"
+    )
+    serch_feilds=(
+        "title",
+        "subtitle",
+        "slug",
+        "body",
+
+    )
+    prepopulated_fields=(
+
+        "slug",
+        "title",
+        "subtitle",
+
+    )
+    date_hierarchy = "publish_date"
+    save_on_top = True
